1e499101717d4ff6f07cd22821b19c7d97079900
Sunny-L <260851801@qq.com>
2016-08-30 22:30:07 +0800

    blog list

--- controllers/index.js
+++ controllers/index.js
@@ -8,6 +8,10 @@ router.get('/about',(req,res,next)=>{
     res.render('about')
 })
 
+router.get('/list',(req,res,next)=>{
+    res.render('list')
+})
+
 router.use((req,res,next)=>{
     res.render('404')
 })
