435c6edc250916724431e131de97369fa1363223
sunny <lixu158@sina.cn>
2016-09-03 23:46:13 +0800

    blog detail update

--- controllers/index.js
+++ controllers/index.js
@@ -12,6 +12,32 @@ router.get('/list',(req,res,next)=>{
     res.render('list')
 })
 
+router.get('/detail/:title',(req,res,next)=>{
+    let tags =[
+        {
+            name:'linux',
+            id:1
+        },{
+            name:'angular.js',
+            id:2
+        },{
+            name:'node.js',
+            id:3
+        },{
+            name:'react.js',
+            id:4
+        },{
+            name:'vue.js',
+            id:5
+        }
+
+    ]
+    res.render('detail',{
+        tags
+    })
+})
+
+
 router.use((req,res,next)=>{
     res.render('404')
 })
